6269226b990254317840a459db0382e3c1440db4
erin dougher <ejdougher@gmail.com>
2020-11-17 08:24:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,23 +2,39 @@ def addition(num1, num2)
 add = num1 + num2
 puts add
 end
-addition(1,2)
-def subtraction(num1, num2)
 
+addition(1,2)
+
+def subtraction(num1, num2)
+sub = num1 - num2
+puts sub
 end
+
+subtraction(3,2)
 
 def division(num1, num2)
-
+div = num1 / num2
+puts div
 end
+
+division(6,2)
 
 def multiplication(num1, num2)
-
+mult = num1 * num2
+puts mult
 end
+
+multiplication(2,3)
 
 def modulo(num1, num2)
-
+mod = num1 % num2
+puts mod
 end
+
+modulo(9,2)
 
 def square_root(num)
-
+Math.sqrt(num)
 end
+
+square_root(81)
